c44ff75d89132dc816129a9f90fe46036d5b3533
AndrewJS21 <TeXHuK917@gmail.com>
2025-12-14 22:32:20 +0700

    Really final fix

--- Todo.SampleApp/Program.cs
+++ Todo.SampleApp/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Todo.SampleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
